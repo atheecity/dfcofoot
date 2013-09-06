0330f5024793a63d26ea55b2650411ef857e25dd
Charles Molin <cmolin21@gmail.com>
2013-09-06 14:28:56 +0200

    Slide accueil

--- src/DF/HomeBundle/Resources/public/js/home.js
+++ src/DF/HomeBundle/Resources/public/js/home.js
@@ -4,7 +4,7 @@ function changFrag()
 		var idFrag = $(this).attr('id');
 		$('.ui-tabs-panel').attr('class', 'ui-tabs-panel ui-tabs-hide');
 		$('#frag'+idFrag).removeAttr('class', 'ui-tabs-hide');
-		$('#frag'+idFrag).attr('class', 'ui-tabs-panel');
+		$('#frag'+idFrag).attr('class', 'ui-tabs-panel select');
 	});
 	
 }
@@ -21,7 +21,15 @@ function sousMenu()
 function autoChangFrag()
 {
 	$('#slide').ready(function() {
-		
+		setInterval(function() {
+			var idFrag = $('.select').attr('id');
+			$('.ui-tabs-panel').attr('class', 'ui-tabs-panel ui-tabs-hide');
+			var id = parseInt(idFrag[4]) + 1;
+			if (id > 4) 
+				id = 1;
+			$('#frag'+id).removeAttr('class', 'ui-tabs-hide');
+			$('#frag'+id).attr('class', 'ui-tabs-panel select');
+		}, 5000);
 	});
 }
 
