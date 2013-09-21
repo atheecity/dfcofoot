140fa316e2bdfc4a6276d32bd62e6c31638e17a9
Charles Molin <cmolin21@gmail.com>
2013-09-21 16:02:22 +0200

    Ajouter but feuille de match

--- src/DF/ManageMatchBundle/Controller/PrivateController.php
+++ src/DF/ManageMatchBundle/Controller/PrivateController.php
@@ -10,8 +10,10 @@ use JMS\SecurityExtraBundle\Annotation\Secure;
 use DF\ManageMatchBundle\Entity\FeuilleMatch;
 use DF\ManageMatchBundle\Entity\Composition;
 use DF\ManageMatchBundle\Entity\Groupe;
+use DF\ManageMatchBundle\Entity\But;
 use DF\ManageMatchBundle\Form\CompositionType;
 use DF\ManageMatchBundle\Form\GroupeType;
+use DF\ManageMatchBundle\Form\ButType;
 
 class PrivateController extends Controller
 {
@@ -51,9 +53,11 @@ class PrivateController extends Controller
     	$em = $this->getDoctrine()->getManager();
     	
     	$match = $em->getRepository('DFMatchBundle:Matchs')->findOneByFeuilleMatch($feuille_id);
+    	$buts = $em->getRepository('DFManageMatchBundle:But')->findByMatch($match);
     	
     	return $this->render('DFManageMatchBundle:Private:displayFeuilleMatch.html.twig', array(
     		'match' => $match,
+    		'buts' => $buts,
     	));
     }
     
@@ -116,6 +120,38 @@ class PrivateController extends Controller
     	return $this->render('DFAdminBundle:Private:form.html.twig', array(
     			'form' => $form->createView(),
     			'title' => 'Groupe'
+    	));
+    }
+    
+    public function newButAction($match_id, $club_id)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$match = $em->getRepository('DFMatchBundle:Matchs')->find($match_id);
+    	$club = $em->getRepository('DFEquipeBundle:Club')->find($club_id);
+    	
+    	$but = new But();
+    	$but->setMatch($match);
+    	$but->setEquipe($club);
+    	
+    	$form = $this->createForm(new ButType(), $but, array('club' => $club_id));
+    	
+    	$request = $this->get('request');
+    	if ($request->getMethod() == 'POST') {
+    		$form->bind($request);
+    		 
+    		if ($form->isValid()) {
+				$em->persist($but);
+    			$em->flush();
+    		}
+    		
+    		return $this->redirect($this->generateUrl('DFManageMatchBundle_displayFeuilleMatch', array(
+    			'feuille_id' => $match->getFeuilleMatch()->getId()
+    		)));
+    	}
+    	
+    	return $this->render('DFAdminBundle:Private:form.html.twig', array(
+    		'form' => $form->createView(),
+    		'title' => 'Ajouter but',
     	));
     }
 }
