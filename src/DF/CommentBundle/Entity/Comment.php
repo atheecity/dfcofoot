79da2e4f6cef322b66c10de3952964b5d0548e76
Charles Molin <cmolin21@gmail.com>
2013-09-27 13:29:27 +0200

    Maj commentaires

--- src/DF/CommentBundle/Entity/Comment.php
+++ src/DF/CommentBundle/Entity/Comment.php
@@ -38,36 +38,6 @@ class Comment extends BaseComment  implements SignedCommentInterface
      * @var User
      */
     protected $author;
-
-    /**
-     * @var integer
-     * 1 : Hors-sujet
-     * 2 : Abusif
-     *
-     * @ORM\Column(name="tag", type="integer", nullable=true)
-     */
-    private $tag;
-    
-    /**
-     * @var string
-     * 
-     * @ORM\Column(name="textAbusif", type="text", nullable=true)
-     */
-    private $textAbusif;
-    
-    /**
-     * @var Integer
-     * 
-     * @ORM\Column(name="nbAbusif", type="integer", nullable=false)
-     */
-    private $nbAbusif = 0;
-    
-    /**
-     * @var Integer
-     * 
-     * @ORM\Column(name="nbHs", type="integer", nullable=false)
-     */
-    private $nbHs = 0;
     
     public function setAuthor(UserInterface $author)
     {
@@ -85,72 +55,8 @@ class Comment extends BaseComment  implements SignedCommentInterface
             return 'Anonymous';
         }
     
-        return $this->getAuthor()->urlizeIdentity();
+        return $this->getAuthor();
     }  
-
-    public function getAuthorNameNotClean()
-    {
-    	if (null === $this->getAuthor()) {
-    		return 'Anonymous';
-    	}
-    	
-    	return $this->getAuthor()->getNom();
-    }
-    
-    public function getAuthorId()
-    {
-    	if (null === $this->getAuthor()) {
-    		return null;
-    	}
-    	
-    	return $this->getAuthor()->getId();
-    }
-
-    /**
-     * Set tag
-     *
-     * @param integer $tag
-     * @return Comment
-     */
-    public function setTag($tag)
-    {
-        $this->tag = $tag;
-
-        return $this;
-    }
-
-    /**
-     * Get tag
-     *
-     * @return integer 
-     */
-    public function getTag()
-    {
-        return $this->tag;
-    }
-
-    /**
-     * Set textAbusif
-     *
-     * @param string $textAbusif
-     * @return Comment
-     */
-    public function setTextAbusif($textAbusif)
-    {
-        $this->textAbusif = $textAbusif;
-
-        return $this;
-    }
-
-    /**
-     * Get textAbusif
-     *
-     * @return string 
-     */
-    public function getTextAbusif()
-    {
-        return $this->textAbusif;
-    }
 
     /**
      * Get id
@@ -161,50 +67,4 @@ class Comment extends BaseComment  implements SignedCommentInterface
     {
         return $this->id;
     }
-
-    /**
-     * Set nbAbusif
-     *
-     * @param integer $nbAbusif
-     * @return Comment
-     */
-    public function setNbAbusif($nbAbusif)
-    {
-        $this->nbAbusif = $nbAbusif;
-
-        return $this;
-    }
-
-    /**
-     * Get nbAbusif
-     *
-     * @return integer 
-     */
-    public function getNbAbusif()
-    {
-        return $this->nbAbusif;
-    }
-
-    /**
-     * Set nbHs
-     *
-     * @param integer $nbHs
-     * @return Comment
-     */
-    public function setNbHs($nbHs)
-    {
-        $this->nbHs = $nbHs;
-
-        return $this;
-    }
-
-    /**
-     * Get nbHs
-     *
-     * @return integer 
-     */
-    public function getNbHs()
-    {
-        return $this->nbHs;
-    }  
 }
